57e933f78af6a55f4b6b6a45b24011f8537c28c5
Gianna Booker <bookerfamily09@gmail.com>
2023-08-03 10:29:46 -0400

    Initial Commit

---
+++ MainPageOfficial/MainPageOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainPageOfficialApp.swift
+//  MainPageOfficial
+//
+//  Created by Scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MainPageOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
